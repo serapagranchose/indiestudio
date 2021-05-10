84679ab9284238e7c13d31e1ab79574889a0e231
Julien <julien.delphine@epitech.eu>
2021-05-10 15:29:10 +0200

    [+] Add bootstrap for test on windows for cmake

---
+++ bootstrap-julien/build/CMakeFiles/bootstrap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bootstrap.dir/src/main.cpp.o"
+  "bootstrap"
+  "bootstrap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bootstrap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
