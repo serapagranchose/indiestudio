71de27a4dae1db0ed51d527e89d821c804de0f97
arthur decovemacker <adecovemacker@localhost.localdomain>
2021-05-25 08:29:17 -0400

    [+] add window creation and map creation

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,74 @@ extern "C" {
     #include <raylib.h>
 }
 
-#include <ostream>
-#include <iostream>
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
 
-int main(int argc, char * argv[]) {
-    std::cout << "! dlrow olleH" << std::endl;
-    Color textColor = LIGHTGRAY;
-    return (0);
+    InitWindow(screenWidth, screenHeight, "BONBERMAN");
+
+    Camera camera = { { 10.0f, 12.0f, 0.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 50.0f, 0 };
+
+    Image image = LoadImage("../map/map_switch.png");
+    Texture2D cubicmap = LoadTextureFromImage(image);
+
+    Mesh mesh = GenMeshCubicmap(image, (Vector3){ 1.0f, 1.0f, 1.0f });
+    Model model = LoadModelFromMesh(mesh);
+
+    Texture2D texture = LoadTexture("../map/textur_2.png");
+    model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
+
+    Vector3 mapPosition = { -14.0f, -5.0f, -3.0f };
+
+    UnloadImage(image);
+
+    SetCameraMode(camera, CAMERA_FREE);
+
+    SetTargetFPS(60);
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        UpdateCamera(&camera);
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            BeginMode3D(camera);
+
+                DrawModel(model, mapPosition, 1.0f, WHITE);
+
+            EndMode3D();
+
+            DrawTextureEx(cubicmap, (Vector2){ screenWidth - cubicmap.width*4.0f - 20, 20.0f }, 0.0f, 4.0f, WHITE);
+            DrawRectangleLines(screenWidth - cubicmap.width*4 - 20, 20, cubicmap.width*4, cubicmap.height*4, GREEN);
+
+            DrawText("SCORE", 658, 90, 10, RED);
+            DrawText("LIFE", 658, 104, 10, RED);
+
+            DrawFPS(10, 10);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    //--------------------------------------------------------------------------------------
+    UnloadTexture(cubicmap);
+    UnloadTexture(texture);
+    UnloadModel(model);
+
+    CloseWindow();
+    //--------------------------------------------------------------------------------------
+
+    return 0;
 }
