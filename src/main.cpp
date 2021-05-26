f0b505a6f6c4a65ade66719871b1e63a5bda6df1
arthur decovemacker <adecovemacker@localhost.localdomain>
2021-05-26 10:18:15 -0400

    [~] change backgroud and camera angle

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 extern "C" {
     #include <raylib.h>
 }
+#include<iostream>
 
 int main(void)
 {
@@ -9,9 +10,9 @@ int main(void)
     const int screenWidth = 1920;
     const int screenHeight = 1080;
 
-    InitWindow(screenWidth, screenHeight, "BONBERMAN");
+    InitWindow(screenWidth, screenHeight, "Anatoly Karpova!!!");
 
-    Camera camera = { { 10.0f, 12.0f, 0.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 50.0f, 0 };
+    Camera camera = { { -4.61976f, 13.6449f, 18.5167f }, { -4.61976f, -10.677f, -1.38298f }, { 0.0f, 1.0f, 0.0f }, 45.0f, 0};
 
     Image image = LoadImage("../map/map_switch.png");
     Texture2D cubicmap = LoadTextureFromImage(image);
@@ -23,11 +24,10 @@ int main(void)
     model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
 
     Vector3 mapPosition = { -14.0f, -5.0f, -3.0f };
-
+    Vector3 cubePosition = { 1.0f, -4.5f, 0.0f };
     UnloadImage(image);
 
     SetCameraMode(camera, CAMERA_FREE);
-
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
 
@@ -43,22 +43,27 @@ int main(void)
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+            ClearBackground(DARKGRAY);
 
             BeginMode3D(camera);
 
                 DrawModel(model, mapPosition, 1.0f, WHITE);
 
+                DrawCube(cubePosition, 1.0f, 1.0f, 1.0f, RED);
+                DrawCubeWires(cubePosition, 1.0f, 1.0f, 1.0f, BLACK);
+
             EndMode3D();
 
             DrawTextureEx(cubicmap, (Vector2){ screenWidth - cubicmap.width*4.0f - 20, 20.0f }, 0.0f, 4.0f, WHITE);
             DrawRectangleLines(screenWidth - cubicmap.width*4 - 20, 20, cubicmap.width*4, cubicmap.height*4, GREEN);
-
             DrawText("SCORE", 658, 90, 10, RED);
             DrawText("LIFE", 658, 104, 10, RED);
-
             DrawFPS(10, 10);
 
+        if (IsKeyDown(KEY_RIGHT)) cubePosition.x += 0.2f;
+        else if (IsKeyDown(KEY_LEFT)) cubePosition.x -= 0.2f;
+        else if (IsKeyDown(KEY_DOWN)) cubePosition.z += 0.2f;
+        else if (IsKeyDown(KEY_UP)) cubePosition.z -= 0.2f;
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
