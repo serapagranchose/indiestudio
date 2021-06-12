615d51f3f9ae93ba67edc423249313dfd4b46fe9
arthur decovemacker <adecovemacker@localhost.localdomain>
2021-06-12 11:40:45 -0400

    [~]change map.txt

---
+++ build/CMakeFiles/bomberman.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bomberman.dir/src/Block.cpp.o"
+  "CMakeFiles/bomberman.dir/src/Button.cpp.o"
+  "CMakeFiles/bomberman.dir/src/Game.cpp.o"
+  "CMakeFiles/bomberman.dir/src/Player.cpp.o"
+  "CMakeFiles/bomberman.dir/src/main.cpp.o"
+  "CMakeFiles/bomberman.dir/src/str_to_word_array.cpp.o"
+  "bomberman"
+  "bomberman.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bomberman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
