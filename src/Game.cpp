4730ca5cd4f6432c68798ae9e8e3e4d45aa753c2
MarcBerthelin <marc.berthelin@gmail.com>
2021-06-12 14:57:55 +0200

    [~] fixed vector errors for windows compiation

--- src/Game.cpp
+++ src/Game.cpp
@@ -23,9 +23,9 @@ Game::Game()
     play.bounds = {this->window.screen_width / 2.0f - play.texture.width / 2.0f, this->window.screen_height / 2.0f - play.texture.height / 2.0f, (float)play.texture.width, (float)play.texture.height};
     this->buttons.push_back(play);
 
-    this->camera.position = (Vector3){0.0f, 10.0f, 10.0f};
-    this->camera.target = (Vector3){0.0f, 0.0f, 0.0f};
-    this->camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+    this->camera.position = Vector3{0.0f, 10.0f, 10.0f};
+    this->camera.target = Vector3{0.0f, 0.0f, 0.0f};
+    this->camera.up = Vector3{0.0f, 1.0f, 0.0f};
     this->camera.fovy = 45.0f;
     this->camera.projection = CAMERA_PERSPECTIVE;
 }
