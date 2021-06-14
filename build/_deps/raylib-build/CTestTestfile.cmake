b364249e1542ad25b556e9f0b682e58341018ba8
marc berthelin <marc.berthelin@epitech.eu>
2021-06-14 09:59:01 +0200

    [-] removing the build directory from the repository

--- build/_deps/raylib-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adecovemacker/tek2/B-YEP-400-LIL-4-1-indiestudio-marc.berthelin/build/_deps/raylib-src
-# Build directory: /home/adecovemacker/tek2/B-YEP-400-LIL-4-1-indiestudio-marc.berthelin/build/_deps/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
